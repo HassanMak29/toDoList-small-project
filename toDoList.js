15ca52a48bb034cf3336324f79dd4a2b3332bde5
HassanMak29 <hassan_makhloufi@yahoo.com>
2021-07-03 23:03:24 +0100

    created toDoList.html toDoList.css and toDoList.js

---
+++ toDoList.js
@@ -0,0 +1,19 @@
+const addToDoButton = document.getElementById('addToDo');
+const inputField = document.getElementById('input-field');
+const toDoContainer = document.getElementById('toDoContainer');
+
+addToDoButton.addEventListener('click', function () {
+    var paragraph = document.createElement('p');
+    toDoContainer.appendChild(paragraph);
+    paragraph.innerText = inputField.value;
+    inputField.value = '';
+    paragraph.classList.add('paragraph-styling');
+
+    paragraph.addEventListener('click', function () {
+        paragraph.style.textDecoration = 'line-through';
+    })
+
+    paragraph.addEventListener('dblclick', function () {
+        toDoContainer.removeChild(paragraph);
+    })
+})
